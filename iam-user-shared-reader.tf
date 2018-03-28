2755f00b9804762ee20108b09b1d7d693ffea8da
Gujarats <gujarat.santana@gmail.com>
2018-03-28 11:27:26 +0700

    change variables names

--- iam-user-shared-reader.tf
+++ iam-user-shared-reader.tf
@@ -1,7 +1,7 @@
-resource "aws_iam_user" "bei-developers"{
+resource "aws_iam_user" "developer"{
   name = "${var.iam-user-s3-reader}"
 }
 
-resource "aws_iam_access_key" "bei-developers-key" {
-  user = "${aws_iam_user.bei-developers.name}"
+resource "aws_iam_access_key" "developers-key" {
+  user = "${aws_iam_user.developer.name}"
 }
