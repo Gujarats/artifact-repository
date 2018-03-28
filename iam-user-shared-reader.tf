b3886ff97e7f27e6d62cdaf1e7d46b8de86c03f8
Gujarats <gujarat.santana@gmail.com>
2018-03-28 14:36:35 +0700

    force user to destroyed

--- iam-user-shared-reader.tf
+++ iam-user-shared-reader.tf
@@ -1,7 +1,4 @@
 resource "aws_iam_user" "developer"{
   name = "${var.iam-user-s3-reader}"
-}
-
-resource "aws_iam_access_key" "developers-key" {
-  user = "${aws_iam_user.developer.name}"
+  force_destroy = true
 }
