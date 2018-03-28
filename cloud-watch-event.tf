ec9c2ccb7cde3d33dc149715dd55dd90d44cc666
Gujarats <gujarat.santana@gmail.com>
2018-03-28 11:32:44 +0700

    fix typo var name

--- cloud-watch-event.tf
+++ cloud-watch-event.tf
@@ -1,8 +1,8 @@
 /*
 This is used to trigger the lambda function
 */
-resource "aws_cloudwatch_event_rule" "bei-schedule-lambda-execution" {
-  name        = "${var.cloud-wath-rule-name}"
+resource "aws_cloudwatch_event_rule" "schedule-lambda-execution" {
+  name        = "${var.cloud-watch-rule-name}"
   description = "schedule lambda execution"
   schedule_expression = "${var.cloud-watch-cron}" 
   role_arn = "${aws_lambda_function.lambda-function-rotate-keys.arn}"
