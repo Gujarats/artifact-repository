bd46215d82aee0a758ccb3eeb8fa7160db2eaf9a
Gujarats <gujarat.santana@gmail.com>
2018-03-28 13:24:48 +0700

    change it ot variables

--- lambda-function.tf
+++ lambda-function.tf
@@ -9,19 +9,19 @@ $ zip aws-rotate-keys.zip ./aws-rotate-keys
 
 resource "aws_lambda_function" "lambda-function-rotate-keys" {
   description = "roate the access & secret keys IAM user"
-  filename         = "aws-roate-keys.zip"
+  filename         = "aws-rotate-keys.zip"
   function_name    = "${var.lambda-function-name}"
   role             = "${aws_iam_role.lambda-role.arn}"
   handler          = "aws-rotate-keys"
   source_code_hash = "${base64sha256(file("aws-rotate-keys.zip"))}"
-  runtime          = "go1.x"
-  timeout = "5"
+  runtime          = "${var.lambda-runtime}"
+  timeout = "${var.lambda-time-out}"
 
   environment {
     variables = {
       PREFIX_ENV_MINE = "MY_APP"
       MYAPP_USER = "${var.iam-user-s3-reader}"
-      MYAPP_REGION = "{var.region}"
+      MYAPP_REGION = "${var.region}"
     }
   }
 }
