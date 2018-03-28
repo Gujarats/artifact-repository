5c34edac2a4473c3183275e5231068a0baa23f65
Gujarats <gujarat.santana@gmail.com>
2018-03-28 12:02:39 +0700

    fix fail plan when using variable to get version

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
     access_key = "${var.aws-access-key}"
     secret_key = "${var.aws-secret-key}"
     region = "${var.aws-region}"
-    version = "${var.aws-version}"
+    version = "~> 1.12"
 }
